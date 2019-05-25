57204b2c782197b1b875e4373bbe13b0f543587b
Tobias Lins <me@tobi.sh>
2019-05-26 00:52:10 +0200

    remake of presence

--- app.js
+++ app.js
@@ -1,106 +1,104 @@
 "use strict";
 
-var currentLogs;
+let currentLogs;
 
-var DEBUG_LOGGING = false;
+const DEBUG_LOGGING = false;
 
 function DefaultSettings() {
-    // If you change a variable here, then also change it in settings.js
-    this.LOG_COUNT_SETTING = 10;
-    this.HOST_CHECK_INTERVAL_SETTING = 5;
-    this.HOST_TIMEOUT_SETTING = 2;
-    this.PORTS_SETTING = "1, 32000";
+  // If you change a variable here, then also change it in settings.js
+  this.LOG_COUNT_SETTING = 10;
+  this.HOST_CHECK_INTERVAL_SETTING = 5;
+  this.HOST_TIMEOUT_SETTING = 2;
+  this.PORTS_SETTING = "1, 32000";
 }
 
-var DEFAULTS = new DefaultSettings();
+const DEFAULTS = new DefaultSettings();
 
 Array.prototype.diff = function(a) {
-    return this.filter(function(i) {return a.indexOf(i) < 0;});
+  return this.filter(function(i) {
+    return a.indexOf(i) < 0;
+  });
 };
 
-module.exports.init = function () {
-    Homey.log("Starting Smart Presence");
-    currentLogs = loadLog();
+module.exports.init = function() {
+  Homey.log("Starting Smart Presence");
+  currentLogs = loadLog();
 };
 
-console.logWithLevel = function (message, level) {
-    if (!level) level = "debug";
-    level = level.toUpperCase();
-    addToLog(new Date(new Date().getTime()), message, level);
+console.logWithLevel = function(message, level) {
+  if (!level) level = "debug";
+  level = level.toUpperCase();
+  addToLog(new Date(new Date().getTime()), message, level);
 };
 
-console.info = function () {
-    console.logWithLevel(join(Array.from(arguments)), "INFO");
-    this.apply(console, arguments)
+console.info = function() {
+  console.logWithLevel(join(Array.from(arguments)), "INFO");
+  this.apply(console, arguments);
 }.bind(console.info);
 
-console.warn = function () {
-    console.logWithLevel(join(Array.from(arguments)), "WARN");
-    this.apply(console, arguments)
+console.warn = function() {
+  console.logWithLevel(join(Array.from(arguments)), "WARN");
+  this.apply(console, arguments);
 }.bind(console.warn);
 
-console.error = function () {
-    console.logWithLevel(join(Array.from(arguments)), "ERROR");
-    this.apply(console, arguments)
+console.error = function() {
+  console.logWithLevel(join(Array.from(arguments)), "ERROR");
+  this.apply(console, arguments);
 }.bind(console.error);
 
-console.log = function () {
-    if(DEBUG_LOGGING === true) {
-        console.logWithLevel(join(Array.from(arguments)), "DEBUG");
-        this.apply(console, arguments)
-    }
+console.log = function() {
+  if (DEBUG_LOGGING === true) {
+    console.logWithLevel(join(Array.from(arguments)), "DEBUG");
+    this.apply(console, arguments);
+  }
 }.bind(console.log);
 
-
 function addToLog(datetime, message, level) {
-    currentLogs.push({
-        datetime: datetime,
-        message: message,
-        level: level
-    });
+  currentLogs.push({
+    datetime: datetime,
+    message: message,
+    level: level
+  });
 
-    var logCount = getLogCount();
+  var logCount = getLogCount();
 
-    if (currentLogs.length > logCount)
-        currentLogs.splice(0, currentLogs.length - logCount);
+  if (currentLogs.length > logCount)
+    currentLogs.splice(0, currentLogs.length - logCount);
 
-    Homey.manager('settings').set('currentLogs', currentLogs);
+  Homey.manager("settings").set("currentLogs", currentLogs);
 }
 
 function getLogCount() {
-    var logCount = parseInt(Homey.manager('settings').get('log_message_count'));
-    if (!logCount)
-        logCount = DEFAULTS.LOG_COUNT_SETTING;
-    return logCount;
+  var logCount = parseInt(Homey.manager("settings").get("log_message_count"));
+  if (!logCount) logCount = DEFAULTS.LOG_COUNT_SETTING;
+  return logCount;
 }
 
 function loadLog() {
-    var currentLogs = Homey.manager('settings').get('currentLogs');
-    if (!currentLogs)
-        currentLogs = [];
-    return currentLogs;
+  var currentLogs = Homey.manager("settings").get("currentLogs");
+  if (!currentLogs) currentLogs = [];
+  return currentLogs;
 }
 
 function join(args) {
-    var toReturn = [];
+  var toReturn = [];
 
-    args.forEach(function (arg) {
-        var builder = [];
-        if (arg instanceof Object) {
-            for (var key in arg) {
-                if(arg.hasOwnProperty(key)) {
-                    builder.push(key + "=" + arg[key])
-                }
-            }
+  args.forEach(function(arg) {
+    var builder = [];
+    if (arg instanceof Object) {
+      for (var key in arg) {
+        if (arg.hasOwnProperty(key)) {
+          builder.push(key + "=" + arg[key]);
         }
-        else {
-            builder.push(arg)
-        }
+      }
+    } else {
+      builder.push(arg);
+    }
 
-        toReturn.push(builder.join(", "))
-    });
+    toReturn.push(builder.join(", "));
+  });
 
-    return toReturn.join(" ");
+  return toReturn.join(" ");
 }
 
 console.debug = console.log;
--- drivers/smart_presence/device.js
+++ drivers/smart_presence/device.js
@@ -1,133 +1,182 @@
-var Network = require("./network.js");
+const Network = require("./network.js");
 
-var UNLOADED_SETTINGS = {error: "unloaded"};
+const UNLOADED_SETTINGS = { error: "unloaded" };
 
 function Device(driver, device_data) {
-    this.className = "Device";
-    this.driver = driver;
-    this.device_data = device_data;
-    this.present = false;
-    // this.driver.exports.setUnavailable(this.device_data, "Away");
-    this.settings = UNLOADED_SETTINGS;
-    this.last_seen_at = new Date().getTime();
+  this.className = "Device";
+  this.driver = driver;
+  this.device_data = device_data;
+  this.present = false;
+  // this.driver.exports.setUnavailable(this.device_data, "Away");
+  this.settings = UNLOADED_SETTINGS;
+  this.last_seen_at = new Date().getTime();
 
-    this.loadSettings(function() {
-        console.log(this.getName(), ": Loaded device with settings", this.settings, "for", this.device_data);
-        if(this.settings == null || !this.settings.hasOwnProperty('away_delay')){
-            console.error('Could not retrieve away delay from settings');
-        }else{
-            this.last_seen_at = new Date().getTime() - (this.settings.away_delay * 1001);
-        }
-    }.bind(this));
+  this.loadSettings(
+    function() {
+      console.log(
+        this.getName(),
+        ": Loaded device with settings",
+        this.settings,
+        "for",
+        this.device_data
+      );
+      if (
+        this.settings == null ||
+        !this.settings.hasOwnProperty("away_delay")
+      ) {
+        console.error("Could not retrieve away delay from settings");
+      } else {
+        this.last_seen_at =
+          new Date().getTime() - this.settings.away_delay * 1001;
+      }
+    }.bind(this)
+  );
 }
 
 Device.prototype.isLoaded = function() {
-    return this.settings !== UNLOADED_SETTINGS;
+  return this.settings !== UNLOADED_SETTINGS;
 };
 
 Device.prototype.loadSettings = function(callback) {
-    this.getSettingsFromHomey(function (err, settings) {
-        this.settings = settings;
-        if(callback) {
-            callback()
-        }
-    }.bind(this));
+  this.getSettingsFromHomey(
+    function(err, settings) {
+      this.settings = settings;
+      if (callback) {
+        callback();
+      }
+    }.bind(this)
+  );
 };
 
-Device.prototype.scan = function (callback) {
-    var network = new Network(this.driver);
-    function isOnline() {
-        delete network;
-        this.setPresent(true);
-        callback(true)
-    }
+Device.prototype.scan = function(callback) {
+  var network = new Network(this.driver);
+  function isOnline() {
+    delete network;
+    this.setPresent(true);
+    callback(true);
+  }
 
-    function isOffline() {
-        delete network;
-        this.setPresent(false);
-        callback(false)
-    }
+  function isOffline() {
+    delete network;
+    this.setPresent(false);
+    callback(false);
+  }
 
-    var timeout = this.getHostTimeoutInMillis();
-    if(this.shouldStressCheck()) {
-        timeout = this.getStressHostTimeoutInMillis();
-    }
+  var timeout = this.getHostTimeoutInMillis();
+  if (this.shouldStressCheck()) {
+    timeout = this.getStressHostTimeoutInMillis();
+  }
 
-    network.scan(this, isOnline.bind(this), isOffline.bind(this), timeout);
+  network.scan(this, isOnline.bind(this), isOffline.bind(this), timeout);
 };
 
-Device.prototype.handleTriggerResult = function (err, result) {
-    if (err)
-        return console.error(err);
+Device.prototype.handleTriggerResult = function(err, result) {
+  if (err) return console.error(err);
 };
 
 /**
  * Sets that the device is present.
  */
-Device.prototype.setPresent = function (present) {
-    this.logReachableLogMessage(present);
+Device.prototype.setPresent = function(present) {
+  this.logReachableLogMessage(present);
 
-    if(present) {
-        var now = new Date().getTime();
-        this.last_seen_at = now;
+  if (present) {
+    var now = new Date().getTime();
+    this.last_seen_at = now;
+  }
+
+  if (!present && this.present && !this.shouldDelayAwayStateSwitch(present)) {
+    this.present = false;
+    this.logDeviceMessage("Left.");
+
+    this.driver.exports.realtime(this.device_data, "alarm_is_home", false);
+
+    Homey.manager("flow").triggerDevice(
+      "user_left",
+      this.getFlowCardTokens(),
+      {},
+      this.device_data,
+      function(err, result) {
+        if (err) return console.error(err);
+      }
+    );
+
+    Homey.manager("flow").trigger(
+      "someone_left",
+      this.getFlowCardTokens(),
+      this.handleTriggerResult
+    );
+    // this.driver.exports.setUnavailable(this.device_data, "Away"); // Commented out cuase this collides with the user specific condition
+
+    if (this.isGuest()) {
+      Homey.manager("flow").trigger(
+        "guest_left",
+        this.getFlowCardTokens(),
+        this.handleTriggerResult
+      );
     }
 
-    if (!present && this.present && !this.shouldDelayAwayStateSwitch(present)) {
-        this.present = false;
-        this.logDeviceMessage("Left.");
-
-        Homey.manager('flow').triggerDevice('user_left', this.getFlowCardTokens(), {}, this.device_data, function (err, result) {
-                if (err) return console.error(err);
-            }
-        );
-
-        Homey.manager('flow').trigger('someone_left', this.getFlowCardTokens(), this.handleTriggerResult);
-        // this.driver.exports.setUnavailable(this.device_data, "Away"); // Commented out cuase this collides with the user specific condition
-
-        if(this.isGuest()) {
-            Homey.manager('flow').trigger('guest_left', this.getFlowCardTokens(), this.handleTriggerResult);
-        }
-
-        if(this.isHouseHoldMember()) {
-            Homey.manager('flow').trigger('household_member_left', this.getFlowCardTokens(), this.handleTriggerResult);
-        }
+    if (this.isHouseHoldMember()) {
+      Homey.manager("flow").trigger(
+        "household_member_left",
+        this.getFlowCardTokens(),
+        this.handleTriggerResult
+      );
     }
-    else if (present && !this.present) {
-        this.present = true;
-        this.logDeviceMessage("Arrived.");
+  } else if (present && !this.present) {
+    this.present = true;
+    this.logDeviceMessage("Arrived.");
 
-        Homey.manager('flow').triggerDevice('user_entered', this.getFlowCardTokens(), {}, this.device_data, function (err, result) {
-                if (err) return console.error(err);
-            }
-        );
+    this.driver.exports.realtime(this.device_data, "alarm_is_home", true);
 
-        Homey.manager('flow').trigger('someone_entered', this.getFlowCardTokens(), this.handleTriggerResult);
-        this.driver.exports.setAvailable(this.device_data);
+    Homey.manager("flow").triggerDevice(
+      "user_entered",
+      this.getFlowCardTokens(),
+      {},
+      this.device_data,
+      function(err, result) {
+        if (err) return console.error(err);
+      }
+    );
 
-        if(this.isGuest()) {
-            Homey.manager('flow').trigger('guest_arrived', this.getFlowCardTokens(), this.handleTriggerResult);
-        }
+    Homey.manager("flow").trigger(
+      "someone_entered",
+      this.getFlowCardTokens(),
+      this.handleTriggerResult
+    );
+    this.driver.exports.setAvailable(this.device_data);
 
-        if(this.isHouseHoldMember()) {
-            Homey.manager('flow').trigger('household_member_arrived', this.getFlowCardTokens(), this.handleTriggerResult);
-        }
+    if (this.isGuest()) {
+      Homey.manager("flow").trigger(
+        "guest_arrived",
+        this.getFlowCardTokens(),
+        this.handleTriggerResult
+      );
     }
+
+    if (this.isHouseHoldMember()) {
+      Homey.manager("flow").trigger(
+        "household_member_arrived",
+        this.getFlowCardTokens(),
+        this.handleTriggerResult
+      );
+    }
+  }
 };
 
 Device.prototype.logReachableLogMessage = function(present) {
-    // Store the last scan result as well so we can show logging if a state changes
-    if(!this.last_present && present) {
-        this.last_present = true;
-        this.logDeviceMessage("Found device.")
-    }
-    else if(this.last_present && !present) {
-        this.last_present = false;
-        this.logDeviceMessage("Can't find device, counting down away delay.")
-    }
+  // Store the last scan result as well so we can show logging if a state changes
+  if (!this.last_present && present) {
+    this.last_present = true;
+    this.logDeviceMessage("Found device.");
+  } else if (this.last_present && !present) {
+    this.last_present = false;
+    this.logDeviceMessage("Can't find device, counting down away delay.");
+  }
 };
 
 Device.prototype.shouldDelayAwayStateSwitch = function() {
-    return this.seenMillisAgo() < this.getAwayDelayInMillis();
+  return this.seenMillisAgo() < this.getAwayDelayInMillis();
 };
 
 /**
@@ -136,9 +185,9 @@ Device.prototype.shouldDelayAwayStateSwitch = function() {
  * @returns {number}
  */
 Device.prototype.seenMillisAgo = function() {
-    var seenAgo = (new Date().getTime() - this.last_seen_at);
-    // this.logDeviceMessage("Device is seen " + seenAgo + " millis ago");
-    return seenAgo;
+  var seenAgo = new Date().getTime() - this.last_seen_at;
+  // this.logDeviceMessage("Device is seen " + seenAgo + " millis ago");
+  return seenAgo;
 };
 
 /**
@@ -146,27 +195,29 @@ Device.prototype.seenMillisAgo = function() {
  * @returns {boolean}
  */
 Device.prototype.shouldStressCheck = function() {
-    var now = new Date().getTime();
-    var predicate = this.present && (this.getAwayDelayInMillis() - (now - this.last_seen_at)) < this.getStressAtInMillis();
+  var now = new Date().getTime();
+  var predicate =
+    this.present &&
+    this.getAwayDelayInMillis() - (now - this.last_seen_at) <
+      this.getStressAtInMillis();
 
-    if(!this.is_stressing && predicate) {
-        this.is_stressing = true;
-        this.logDeviceMessage("Start stressing")
-    }
-    else if(this.is_stressing && !predicate) {
-        this.is_stressing = false;
-        this.logDeviceMessage("Stop stressing")
-    }
+  if (!this.is_stressing && predicate) {
+    this.is_stressing = true;
+    this.logDeviceMessage("Start stressing");
+  } else if (this.is_stressing && !predicate) {
+    this.is_stressing = false;
+    this.logDeviceMessage("Stop stressing");
+  }
 
-    return predicate;
+  return predicate;
 };
 
 /**
  * Returns true if the device is present, returns false otherwise.
  * @returns {boolean}
  */
-Device.prototype.isPresent = function () {
-    return this.present;
+Device.prototype.isPresent = function() {
+  return this.present;
 };
 
 /**
@@ -174,103 +225,112 @@ Device.prototype.isPresent = function () {
  * @returns {string}
  */
 Device.prototype.getDeviceID = function() {
-    return this.device_data.id;
+  return this.device_data.id;
 };
 
 Device.prototype.getSettingsFromHomey = function(callback) {
-    this.driver.exports.getSettings(this.device_data, callback);
+  this.driver.exports.getSettings(this.device_data, callback);
 };
 
 /**
  * Returns the stored settings for this Device.
  * @returns {Object}
  */
-Device.prototype.getSettings = function () {
-    return this.settings;
+Device.prototype.getSettings = function() {
+  return this.settings;
 };
 
 Device.prototype.logDeviceMessage = function(message) {
-    console.info(this.getName(), ":", message)
+  console.info(this.getName(), ":", message);
 };
 
 /**
  * Returns the host on which the device is active if it's nearby.
  * @returns {*|string|string}
  */
-Device.prototype.getHost = function () {
-    return this.getSettings().host;
+Device.prototype.getHost = function() {
+  return this.getSettings().host;
 };
 
 /**
  * Returns the away delay in millis.
  * @returns {number}
  */
-Device.prototype.getAwayDelayInMillis = function () {
-    var settings = this.getSettings();
-    if(settings) {
-        return parseInt(settings.away_delay) * 1000;
-    }
-    else {
-        var defaultAwayDelay = 900 * 1000;
-        console.log("Using a default of ", defaultAwayDelay, "millis for 'away_delay', as the settings are not loaded yet.");
-        return defaultAwayDelay;
-    }
+Device.prototype.getAwayDelayInMillis = function() {
+  var settings = this.getSettings();
+  if (settings) {
+    return parseInt(settings.away_delay) * 1000;
+  } else {
+    var defaultAwayDelay = 900 * 1000;
+    console.log(
+      "Using a default of ",
+      defaultAwayDelay,
+      "millis for 'away_delay', as the settings are not loaded yet."
+    );
+    return defaultAwayDelay;
+  }
 };
-
 
 /**
  * When should we start stressing for a host? The setting is in millis.
  * The value represents a time which we must start stressing before a device is marked away.
  * @returns {number}
  */
-Device.prototype.getStressAtInMillis = function () {
-    var settings = this.getSettings();
-    if(settings) {
-        return parseInt(settings.start_stressing_at) * 1000;
-    }
-    else {
-        var defaultStress = 0;
-        console.log("Using a default of ", defaultStress, "millis for 'start_stressing_at', as the settings are not loaded yet.");
-        return defaultStress;
-    }
+Device.prototype.getStressAtInMillis = function() {
+  var settings = this.getSettings();
+  if (settings) {
+    return parseInt(settings.start_stressing_at) * 1000;
+  } else {
+    var defaultStress = 0;
+    console.log(
+      "Using a default of ",
+      defaultStress,
+      "millis for 'start_stressing_at', as the settings are not loaded yet."
+    );
+    return defaultStress;
+  }
 };
 
-
-Device.prototype.getHostTimeoutInMillis = function () {
-    var settings = this.getSettings();
-    if(settings) {
-        return parseInt(settings.host_timeout) * 1000;
-    }
-    else {
-        var defaultHostTimeout = 2000;
-        console.log("Using a default of ", defaultHostTimeout, "millis for 'host_timeout', as the settings are not loaded yet.");
-        return defaultHostTimeout;
-    }
+Device.prototype.getHostTimeoutInMillis = function() {
+  var settings = this.getSettings();
+  if (settings) {
+    return parseInt(settings.host_timeout) * 1000;
+  } else {
+    var defaultHostTimeout = 2000;
+    console.log(
+      "Using a default of ",
+      defaultHostTimeout,
+      "millis for 'host_timeout', as the settings are not loaded yet."
+    );
+    return defaultHostTimeout;
+  }
 };
 
-
-Device.prototype.getStressHostTimeoutInMillis = function () {
-    var settings = this.getSettings();
-    if(settings) {
-        return parseInt(settings.stress_host_timeout) * 1000;
-    }
-    else {
-        var defaultHostTimeout = 2000;
-        console.log("Using a default of ", defaultHostTimeout, "millis for 'stress_host_timeout', as the settings are not loaded yet.");
-        return defaultHostTimeout;
-    }
+Device.prototype.getStressHostTimeoutInMillis = function() {
+  var settings = this.getSettings();
+  if (settings) {
+    return parseInt(settings.stress_host_timeout) * 1000;
+  } else {
+    var defaultHostTimeout = 2000;
+    console.log(
+      "Using a default of ",
+      defaultHostTimeout,
+      "millis for 'stress_host_timeout', as the settings are not loaded yet."
+    );
+    return defaultHostTimeout;
+  }
 };
 
 /**
  * Gets the name of the device in Homey (which is most likely the name of a person).
  */
 Device.prototype.getName = function() {
-    var settings = this.getSettings();
-    if(settings) {
-        return this.getSettings().name
-    } else {
-        return this.getDeviceID();
-    }
+  var settings = this.getSettings();
+  if (settings) {
+    return this.getSettings().name;
+  } else {
+    return this.getDeviceID();
+  }
 };
 
 /**
@@ -278,23 +338,23 @@ Device.prototype.getName = function() {
  * @returns {{who}}
  */
 Device.prototype.getFlowCardTokens = function() {
-    return { "who" : this.getName() };
+  return { who: this.getName() };
 };
 
 /**
  * Returns true if this is a household member device.
  * @returns {boolean}
  */
-Device.prototype.isHouseHoldMember = function () {
-    return this.settings.is_guest === false;
+Device.prototype.isHouseHoldMember = function() {
+  return this.settings.is_guest === false;
 };
 
 /**
  * Returns true if this is a guest device.
  * @returns {boolean}
  */
-Device.prototype.isGuest = function () {
-    return this.settings.is_guest === true;
+Device.prototype.isGuest = function() {
+  return this.settings.is_guest === true;
 };
 
 module.exports = Device;
